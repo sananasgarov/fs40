847c233a4a4cf4b873f1f92c15859bc628710f2e
sananasgarov <prog58816@gmail.com>
2025-06-19 16:50:51 +0400

    dom

---
+++ jstask/Dom/main.js
@@ -0,0 +1,32 @@
+// task01
+// let a = document.querySelector("li:first-child")
+// console.log(a);
+
+// task02
+// let h1 = document.querySelector("h1")
+// h1.remove()
+// let ulelem = document.querySelector("ul")
+// let newli = document.createElement("li")
+// newli.innerText = "TITAN RTX"
+// ulelem.append(newli)
+
+// task03
+// let h1 = document.querySelector("h1")
+// h1.innerHTML = "GeForce 20 Series";
+// h1.style.color = "#ebebeb";
+
+// task04
+// let a = document.querySelectorAll(".primary")
+// a.forEach(item => {
+//     console.log(item);
+
+// })
+
+// task05
+// let li = document.querySelectorAll("li");
+// li.forEach((item) => {
+//   item.classList.add("item");
+//   if (!item.classList.contains("primary")) {
+//     item.classList.add("secondary");
+//   }
+// });
--- jstask/jsobj/main.js
+++ jstask/jsobj/main.js
@@ -1,37 +1,47 @@
-let obj = {
-  sum: function sum(a, b) {
-    a = +a
-    b = +b
-    return (obj.result = a + b);
-  },
-  o: function o(a, b) {
-    return (obj.result = a - b);
-  },
-  m: function m(a, b) {
-    return (obj.result = a * b);
-  },
-   d: function d(a, b) {
-    return (obj.result = a / b);
-  },
-};
-op = prompt("operator daxil edin")
-num1 = prompt("eded daxil edin")
-num2 = prompt("eded daxil edin")
-switch (op) {
-    case "+":
-        obj.sum(num1,num2)
-        break;
-    case "-":
-        obj.o(num1,num2)
-        break;
-    case "*":
-        obj.m(num1,num2)
-        break;
-    case "/":
-        obj.d(num1,num2)
-        break;
-    default:
-        throw new Error("");
+// task05
+// let obj = {
+//   sum: function sum(a, b) {
+//     a = +a
+//     b = +b
+//     return (obj.result = a + b);
+//   },
+//   o: function o(a, b) {
+//     return (obj.result = a - b);
+//   },
+//   m: function m(a, b) {
+//     return (obj.result = a * b);
+//   },
+//    d: function d(a, b) {
+//     return (obj.result = a / b);
+//   },
+// };
+// op = prompt("operator daxil edin")
+// num1 = prompt("eded daxil edin")
+// num2 = prompt("eded daxil edin")
+// switch (op) {
+//     case "+":
+//         obj.sum(num1,num2)
+//         break;
+//     case "-":
+//         obj.o(num1,num2)
+//         break;
+//     case "*":
+//         obj.m(num1,num2)
+//         break;
+//     case "/":
+//         obj.d(num1,num2)
+//         break;
+//     default:
+//         throw new Error("");
+// }
+// console.log(obj);
+// console.log(obj.result);
+
+// task06
+function mostFrequent(str) {
+  let arr = str.split(", ");
+  return arr.sort((a, b) =>
+    arr.filter(x => x === b).length - arr.filter(x => x === a).length
+  )[0];
 }
-console.log(obj);
-console.log(obj.result);
+console.log(mostFrequent("1, 1, 2, 3, 4, 1, 5, 1"));
